4e601903c58f9d6d3e592fb8208ed6340fbcc75e
minhaj-mhd <minhajmuhamad@gmail.com>
2024-05-12 14:30:00 +0530

    Template rendering setup

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from app.schemas import StockIn
 from app.services.data_service import fetch_stock_data
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="Stock Prediction API")
+
+templates = Jinja2Templates(directory="app/templates")
+
 
 @app.post("/predict")
 async def predict(stock_data: StockIn):
     df = fetch_stock_data(stock_data.ticker, stock_data.period, stock_data.interval)
     return {"data": df.shape}
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
