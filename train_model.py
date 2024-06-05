21297afe8956d3a06e927b9dee37b4a002464cd3
minhaj-mhd <minhajmuhamad@gmail.com>
2024-06-05 14:30:00 +0530

    Model training

---
+++ train_model.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+
+def create_model(input_shape):
+    model = Sequential()
+    model.add(LSTM(50, return_sequences=True, input_shape=input_shape))
+    model.add(LSTM(50, return_sequences=False))
+    model.add(Dense(25))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
